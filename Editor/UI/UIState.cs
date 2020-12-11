c3ac9687345d34db4bcdf0099dde288a9c31e4d0
katana7755 <jaeho@unity3d.com>
2020-12-12 03:37:40 +0900

    Printing total collected memory size

--- Editor/UI/UIState.cs
+++ Editor/UI/UIState.cs
@@ -72,7 +72,7 @@ namespace Unity.MemoryProfiler.Editor.UI
                 m_Tables = copy.m_Tables;
             }
 
-            public abstract ViewPane GetDefaultView(UIState uiState, IViewPaneEventListener viewPaneEventListener);
+            public abstract ViewPane GetDefaultView(UIState uiState, IViewPaneEventListener viewPaneEventListener, UnityEngine.Experimental.UIElements.VisualElement toolbarExtension);
 
             public int GetTableIndex(Database.Table tab)
             {
@@ -235,10 +235,10 @@ namespace Unity.MemoryProfiler.Editor.UI
                 }
             }
 
-            public override ViewPane GetDefaultView(UIState uiState, IViewPaneEventListener viewPaneEventListener)
+            public override ViewPane GetDefaultView(UIState uiState, IViewPaneEventListener viewPaneEventListener, UnityEngine.Experimental.UIElements.VisualElement toolbarExtension)
             {
                 if (uiState.snapshotMode != null && uiState.snapshotMode.snapshot != null)
-                    return new UI.TreeMapPane(uiState, viewPaneEventListener);
+                    return new UI.TreeMapPane(uiState, viewPaneEventListener, toolbarExtension);
                 else
                     return null;
             }
@@ -321,7 +321,7 @@ namespace Unity.MemoryProfiler.Editor.UI
                 UpdateTableSelectionNamesFromSchema(m_ObjectDataFormatter, m_SchemaDiff);
             }
 
-            public override ViewPane GetDefaultView(UIState uiState, IViewPaneEventListener viewPaneEventListener)
+            public override ViewPane GetDefaultView(UIState uiState, IViewPaneEventListener viewPaneEventListener, UnityEngine.Experimental.UIElements.VisualElement toolbarExtension)
             {
                 //TODO: delete this method once the default for diff is treemap
                 Database.Table table = null;
